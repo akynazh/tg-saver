41042b7aa87cc7c9ed2be663988fd967ac8dea2b
jiangzhh <akynazh@qq.com>
2023-09-09 20:58:53 +0800

    re

--- database.py
+++ database.py
@@ -32,27 +32,32 @@ class BotDb:
                 conn.close()
 
     def insert_sgp_av(self, sgp_av, cur):
-        cur.execute("INSERT INTO t_tg_sgp(av_id, title, file_id, msg_id) VALUES(?, ?, ?, ?)",
-                    (sgp_av["av_id"], sgp_av["title"], sgp_av["file_id"], sgp_av["msg_id"]))
+        cur.execute("SELECT av_id FROM t_tg_sgp where av_id = ?", (sgp_av["av_id"],))
+        if not cur.fetchone():
+            cur.execute("INSERT INTO t_tg_sgp(av_id, title, file_id, msg_id) VALUES(?, ?, ?, ?)",
+                        (sgp_av["av_id"], sgp_av["title"], sgp_av["file_id"], sgp_av["msg_id"]))
 
     def insert_cav(self, cav, cur):
         cur.execute("INSERT INTO t_tg_cav(title, file_id, msg_id) VALUES(?, ?, ?)",
                     (cav["title"], cav["file_id"], cav["msg_id"]))
 
     def batch_insert_medias(self, media_list: list, media_type: int):
+        LOG.info(f"开始保存记录到数据库, 即将保存条数: {len(media_list)}")
         conn = None
         try:
             conn = self.get_conn()
             cur = conn.cursor()
-            for v in media_list:
+            for m in media_list:
+                LOG.info(f"插入数据: {m}")
                 if media_type == BotDb.TYPE_SGP:
-                    self.insert_sgp_av(v, cur)
+                    self.insert_sgp_av(m, cur)
                 elif media_type == BotDb.TYPE_CAV:
-                    self.insert_cav(v, cur)
+                    self.insert_cav(m, cur)
             conn.commit()
+            LOG.info(f"成功保存记录到数据库, 已保存条数: {len(media_list)}")
             return True
         except Exception as e:
-            LOG.error(e)
+            LOG.error(f"保存记录到数据库失败: {e}")
         finally:
             if conn:
                 conn.close()
--- handlers.py
+++ handlers.py
@@ -1,7 +1,9 @@
 from database import BotDb
+import re
 
 
 class VideoHandler:
+    AV_PAT = re.compile(r"[a-z0-9]+[-_](?:ppv-)?[a-z0-9]+")
 
     def __init__(self, title: str, file_id: str, msg_id: int, video_type: int):
         self.title = title
@@ -9,7 +11,7 @@ class VideoHandler:
         self.msg_id = msg_id
         self.video_type = video_type
 
-    def get_cav_video(self) -> dict:
+    def get_cav_video(self):
         return {
             "title": self.title,
             "file_id": self.file_id,
@@ -17,29 +19,25 @@ class VideoHandler:
         }
 
     def get_sgp_video(self):
-        # 取得番号
-        t1 = self.title.find("<")
-        t2 = self.title.find(">")
-        if t1 != -1 and t2 != -1:
-            av_id = self.title[t1 + 1:t2]
-        else:
-            av_id = "-"
+        ids = VideoHandler.AV_PAT.findall(self.title.lower())
+        if not ids or len(ids) == 0:
+            return None
         # 添加 av
         return {
-            "av_id": av_id,
+            "av_id": ids[0],
             "title": self.title,
             "file_id": self.file_id,
             "msg_id": self.msg_id
         }
 
-    def get_video_by_type(self) -> dict:
+    def get_video_by_type(self):
         try:
             if self.video_type == BotDb.TYPE_SGP:
                 return self.get_sgp_video()
             elif self.video_type == BotDb.TYPE_CAV:
                 return self.get_cav_video()
         except Exception:
-            return {}
+            return None
 
 
 class PhotoHandler:
