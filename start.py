0b14152fe9d0e5601fd6f631d62e9c75cc31f3c6
jiangzhh <akynazh@qq.com>
2023-10-03 00:39:13 +0800

    update

---
+++ start.py
@@ -0,0 +1,8 @@
+import os
+import time
+
+for i in range(300):
+    print(f"# 第 {i} 次醒来 >_< #")
+    os.system("python3 saver.py -fc DoO_o -tc zh_coav_channel_1 -ft 1 -lm 5")
+    print("# 睡会觉 =_= #")
+    time.sleep(6)
