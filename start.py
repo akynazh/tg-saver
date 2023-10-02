0fdc7896d5e7cf05a82d972cfc99a078d520b765
jiangzhh <akynazh@qq.com>
2023-10-03 00:41:40 +0800

    update

--- start.py
+++ start.py
@@ -1,8 +1,9 @@
 import os
 import time
+import logging
 
 for i in range(300):
-    print(f"# 第 {i} 次醒来 >_< #")
+    logging.info(f"# 第 {i} 次醒来 >_< #")
     os.system("python3 saver.py -fc DoO_o -tc zh_coav_channel_1 -ft 1 -lm 5")
-    print("# 睡会觉 =_= #")
+    logging.info("# 睡会觉 =_= #")
     time.sleep(6)
