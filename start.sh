32c8bb6242ac0515dff8a75e24616dd6628af185
jiangzhh <akynazh@qq.com>
2023-10-07 00:18:27 +0800

    update

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #!/bin/zsh
 
-cd ~/Codes/tg-saver && python3 -m utils.clean
-
 python3 ~/Codes/tg-saver/saver.py -fc DoO_o -tc zh_coav_channel_1 -ft 1 -lm 100
 sleep 60
 python3 ~/Codes/tg-saver/saver.py -fc youzhi7777 -tc zh_coav_channel_1 -ft 1 -lm 100
@@ -10,4 +8,5 @@ python3 ~/Codes/tg-saver/saver.py -fc shuiguopai -tc zh_sgp_av_channel_1 -ft 1 -
 sleep 180
 python3 ~/Codes/tg-saver/saver.py -fc Zhangzhoulao666 -tc zh_jav_channel_1 -ft 0 -ct 102 -lm 500
 
+cd ~/Codes/tg-saver && python3 -m utils.clean
 cd ~/Codes/tg-saver && python3 -m utils.stat
