37113d82315f3173d0268853afa3fbd0a001bb19
akynazh <akynazh@qq.com>
2023-10-07 23:56:09 +0900

    update

--- start.sh
+++ start.sh
@@ -1,12 +1,16 @@
 #!/bin/zsh
 
+python3 ~/Codes/tg-saver/saver.py -fc shuiguopai -tc zh_sgp_av_channel_1 -ft 1 -ct 101 -rn
+python3 ~/Codes/tg-saver/saver.py -fc Zhangzhoulao666 -tc zh_jav_channel_1 -ft 0 -ct 102 -rn
+sleep 5
 python3 ~/Codes/tg-saver/saver.py -fc DoO_o -tc zh_coav_channel_1 -ft 1 -lm 100
 sleep 60
 python3 ~/Codes/tg-saver/saver.py -fc youzhi7777 -tc zh_coav_channel_1 -ft 1 -lm 100
-sleep 120
-python3 ~/Codes/tg-saver/saver.py -fc shuiguopai -tc zh_sgp_av_channel_1 -ft 1 -ct 101 -rn
-sleep 180
-python3 ~/Codes/tg-saver/saver.py -fc Zhangzhoulao666 -tc zh_jav_channel_1 -ft 0 -ct 102 -lm 500
+
+
+
+
+
 
 cd ~/Codes/tg-saver && python3 -m utils.clean
 cd ~/Codes/tg-saver && python3 -m utils.stat
