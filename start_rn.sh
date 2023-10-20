db36c516487a2f8ccae6ccdfc7866c44e02fa86c
jiangzhh <akynazh@qq.com>
2023-10-20 22:56:52 +0800

    update

--- start_rn.sh
+++ start_rn.sh
@@ -1,9 +1,5 @@
 #!/bin/zsh
 
-python3 ~/Codes/tg-saver/saver.py -fc avling -tc zh_coav_channel_1 -ft 1 -lm 300
-sleep 30
-python3 ~/Codes/tg-saver/saver.py -fc dakada -tc zh_coav_channel_1 -ft 1 -lm 300
-sleep 30
 python3 ~/Codes/tg-saver/saver.py -fc shuiguopai -tc zh_sgp_av_channel_1 -ft 1 -ct 101 -rn
 sleep 3
 python3 ~/Codes/tg-saver/saver.py -fc Zhangzhoulao666 -tc zh_jav_channel_1 -ft 0 -ct 102 -rn
