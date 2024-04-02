cd628692c62aa0c970a74f2c55f51b5c1520ed52
jiangzhh <akynazh@gmail.com>
2024-04-02 22:41:27 +0800

    feat: check

--- utils/check.py
+++ utils/check.py
@@ -1,4 +1,4 @@
-# python3 -m utils.clean xxx
+# python3 -m utils.check xxx
 import sqlite3
 import common
 import sys
@@ -9,22 +9,13 @@ if len(sys.argv) > 1:
 
 LOG = common.Logger(path_log_file=f"{common.PATH_ROOT}/log.txt").logger
 CONN = sqlite3.connect(common.CFG.db_file)
-
-
-CONN.cursor().execute(
-    f"""
-delete from t_tg_zh_coav_channel_1 where content = '{content}';
-"""
-)
 res = (
     CONN.cursor()
     .execute(
-        """
-select changes();
+        f"""
+select count(*) from t_tg_zh_coav_channel_1 where content = '{content}';
 """
     )
     .fetchone()
 )
-CONN.commit()
-
-LOG.info(f"clean t_tg_zh_coav_channel_1: {res[0]}")
+LOG.info(f"clean t_tg_zh_coav_channel_1: {res}")
