ee04b9f681a3cccae07e8df5831197160e59b21d
jiangzhh <akynazh@gmail.com>
2024-04-02 22:53:23 +0800

    feat: clean

--- utils/clean.py
+++ utils/clean.py
@@ -3,19 +3,21 @@ import sqlite3
 import common
 import sys
 
-content = "-"
-if len(sys.argv) > 1:
-    content = sys.argv[1]
-
 LOG = common.Logger(path_log_file=f"{common.PATH_ROOT}/log.txt").logger
 CONN = sqlite3.connect(common.CFG.db_file)
 
-
-CONN.cursor().execute(
-    f"""
-delete from t_tg_zh_coav_channel_1 where content = '{content}';
-"""
-)
+if len(sys.argv) > 1:
+    CONN.cursor().execute(
+        f"""
+    delete from t_tg_zh_coav_channel_1 where instr(content, '{sys.argv[1]}');
+    """
+    )
+else:
+    CONN.cursor().execute(
+        f"""
+    delete from t_tg_zh_coav_channel_1 where content = '-';
+    """
+    )
 res = (
     CONN.cursor()
     .execute(
