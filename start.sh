5f87abf321fb4ef606d335087933fa131a9060a9
jiangzhh <akynazh@gmail.com>
2024-04-04 14:13:00 +0800

    update

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 #!/bin/zsh
 
 python3 ~/Codes/tg-saver/saver.py -fc guochanhaose -tc zh_coav_channel_1 -ft 1 -lm 15
-python3 ~/Codes/tg-saver/saver.py -fc tgthflj -tc zh_coav_channel_1 -ft 1 -lm 15
 
 # cd ~/Codes/tg-saver && python3 -m utils.stat
 cd ~/Codes/tg-saver && python3 -m utils.clean
