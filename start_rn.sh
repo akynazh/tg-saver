6e97ff2c84eac55b8dc225900286e96212262a1e
jiangzhh <akynazh@gmail.com>
2024-04-04 14:11:50 +0800

    update

--- start_rn.sh
+++ start_rn.sh
@@ -1,17 +1,15 @@
 #!/bin/zsh
 
-python3 ~/Codes/tg-saver/saver.py -fc shuiguopai -tc zh_sgp_av_channel_1 -ft 1 -ct 101 -rn
-sleep 3
-python3 ~/Codes/tg-saver/saver.py -fc Zhangzhoulao666 -tc zh_jav_channel_1 -ft 0 -ct 102 -rn
-sleep 3
-python3 ~/Codes/tg-saver/saver.py -fc DoO_o -tc zh_coav_channel_1 -ft 1 -rn
-sleep 3
-python3 ~/Codes/tg-saver/saver.py -fc youzhi7777 -tc zh_coav_channel_1 -ft 1 -rn
-sleep 3
-python3 ~/Codes/tg-saver/saver.py -fc avling -tc zh_coav_channel_1 -ft 1 -rn
-sleep 3
-python3 ~/Codes/tg-saver/saver.py -fc dakada -tc zh_coav_channel_1 -ft 1 -rn
+# python3 ~/Codes/tg-saver/saver.py -fc shuiguopai -tc zh_sgp_av_channel_1 -ft 1 -ct 101 -rn
+# sleep 3
+# python3 ~/Codes/tg-saver/saver.py -fc Zhangzhoulao666 -tc zh_jav_channel_1 -ft 0 -ct 102 -rn
+# sleep 3
+# python3 ~/Codes/tg-saver/saver.py -fc DoO_o -tc zh_coav_channel_1 -ft 1 -rn
+# sleep 3
+# python3 ~/Codes/tg-saver/saver.py -fc youzhi7777 -tc zh_coav_channel_1 -ft 1 -rn
+# sleep 3
+# python3 ~/Codes/tg-saver/saver.py -fc avling -tc zh_coav_channel_1 -ft 1 -rn
 
 
-cd ~/Codes/tg-saver && python3 -m utils.stat
+# cd ~/Codes/tg-saver && python3 -m utils.stat
 cd ~/Codes/tg-saver && python3 -m utils.clean
