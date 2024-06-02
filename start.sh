2c76bc699d1e6fe60f33f061e22a6c2e56b92b2b
jiangzhh <akynazh@gmail.com>
2024-06-02 23:33:59 +0800

    update

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 #!/bin/zsh
 
 path_py=~/Codes/tg-saver/.venv/bin/python3
-$path_py ~/Codes/tg-saver/saver.py -fc FanChPa -tc zh_coav_channel_1 -ft 1 -lm 15
 
 # cd ~/Codes/tg-saver && $path_py -m utils.stat
 cd ~/Codes/tg-saver && $path_py -m utils.clean
--- start_rn.sh
+++ start_rn.sh
@@ -8,6 +8,8 @@ $path_py ~/Codes/tg-saver/saver.py -fc SGP95 -tc zh_sgp_av_channel_1 -ft 1 -rn
 sleep 3
 $path_py ~/Codes/tg-saver/saver.py -fc ribenav8899 -tc zh_jav_channel_1 -ft 1 -ct 102 -rn
 sleep 3
+$path_py ~/Codes/tg-saver/saver.py -fc FanChPa -tc zh_coav_channel_1 -ft 1 -rn
+sleep 3
 
 # cd ~/Codes/tg-saver && $path_py -m utils.stat
 cd ~/Codes/tg-saver && $path_py -m utils.clean
